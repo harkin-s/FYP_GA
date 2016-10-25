208b4958822b1a084ed4635a8c53d0e492e8449b
Sean Harkin <sharkin104@gmail.com>
2016-10-25 15:21:28 +0100

    Fixed out of bounds error changed the structure of the

--- GA/GeneticAlgorithm.cs
+++ GA/GeneticAlgorithm.cs
@@ -15,31 +15,29 @@ namespace GA
         public static bool ideal = false;
         public static int perfectGen = 0;
         public static int bestGlobalScore = 0;
-        public static int ALLELES = 4;
+        public static int ALLELES = 12;
         public static int fitnessWeight = 1;
         public static int phenotypicWeight = 1;
         public static readonly int POPULATION = 100;
-        public static readonly int GENES = 8;
+        public static readonly int GENES = 24;
         public static List<Organism> GENERATION = new List<Organism>();
 
 
         // Main method call the generate population method and all proccedding methods
         static void Main(string[] args)
         {
-            //int[,] gen = new int[POPULATION, GENES];
-            
             populate();
-            getNextGen();
-            var count = 0; 
+            var count = 0;
             while (!ideal)
             {
-               getNextGen();
-               count++;
+                getNextGen();
+                count++;
 
             }
             Console.WriteLine("IT TOOK --->" + count + "<---- GENERATIONS");
             //printOrg(gen, perfectGen)
         }
+
         //Populates the array with random 1's and 0's
         public static void populate()
         {
@@ -54,7 +52,7 @@ namespace GA
                     GENERATION[x].genes.SetValue(num, y);
                 }
             }
-            
+
         }
         //Used to get the next generation 
         public static void getNextGen()
@@ -63,6 +61,7 @@ namespace GA
             List<Organism> tempGen = new List<Organism>();
             var compSize = 4;
             int[,] tourn = new int[compSize, GENES];
+            var bestPerformer = 0;
             //Uses tournment select too fill the next population 
 
             //Adds random Oragnisms to the test population.
@@ -71,7 +70,7 @@ namespace GA
                 // tempGen.Add(new Organism(GENES));
                 var score = 0;
                 var bestScore = 0;
-                var bestPerformer = 0;
+                bestPerformer = 0;
                 // nextGen.Add(new Organism(GENES));
                 for (var a = 0; a < compSize; a++)
                 {
@@ -80,8 +79,6 @@ namespace GA
                     for (var c = 0; c < GENES; c++)
                     {
                         score = score + GENERATION[sel].genes[c];
-                                        
-
                     }
                     GENERATION[sel].fitness = score;
                     if (score > bestScore)
@@ -89,28 +86,55 @@ namespace GA
                         bestScore = score;
                         bestPerformer = sel;
                     }
-                    if(score == GENES)
+                    if (score == GENES)
                     {
                         ideal = true;
                         printOrg(sel);
                     }
                 }
 
-                //Calculate Genotype
+                var x = 0;
+                var y = 1;
+                GENERATION[bestPerformer].phenotype = "";
+                for (int b = 0; b < ALLELES; b++)
+                {
 
+                    if (b > 0)
+                    {
+                        x = x + 2;
+                        y = y + 2;
+                    }
+                    var gene = GENERATION[bestPerformer].genes[x].ToString() + GENERATION[bestPerformer].genes[y].ToString();
+
+                    if (gene.Equals("00"))
+                    {
+                        GENERATION[bestPerformer].phenotype = GENERATION[bestPerformer].phenotype + "a";
+                    }
+                    else if (gene.Equals("01"))
+                    {
+                        GENERATION[bestPerformer].phenotype = GENERATION[bestPerformer].phenotype + "b";
+                    }
+                    else if (gene.Equals("10"))
+                    {
+                        GENERATION[bestPerformer].phenotype = GENERATION[bestPerformer].phenotype + "c";
+                    }
+                    else if (gene.Equals("11"))
+                    {
+                        GENERATION[bestPerformer].phenotype = GENERATION[bestPerformer].phenotype + "d";
+                    }
+                    else
+                    {
+                        throw new System.ArgumentException("ERROR!! -- Gene pair not found");
+                    }
+                }
                 tempGen.Add(GENERATION[bestPerformer]);
-                //printOrg(bestPerformer);
             }
-
-            // Console.WriteLine("Temp gen has been filled \n " + tempGen.Count);
             GENERATION = generateNextGen(tempGen);
         }
 
         // Use the temp population to get the next gerneration 
         public static List<Organism> generateNextGen(List<Organism> gen)
         {
-            //Add random mutaion latter
-           
             var gene = 0;
             //Cross over first 
             List<Organism> nextGen = new List<Organism>();
@@ -137,88 +161,62 @@ namespace GA
 
                     if (random == 1)
                     {
-                        nextGen[pop].genes.SetValue(b, nextGen[pop].genes[b] == 1 ? 1 : 0);
+                        if(nextGen[pop].genes[b] == 0)
+                        {
+                            nextGen[pop].genes[b] = 1;
+                        }
+                        else
+                        {
+                            nextGen[pop].genes[b] = 0;
+                        }
                         Console.WriteLine("Mutate");
                     }
 
                 }
             }
             return nextGen;
-      
+
         }
 
-        public static int getPartner(List<Organism> gen ,int sel)
+        public static int getPartner(List<Organism> gen, int sel)
         {
             var testSize = 4;
             var bestPartner = 0;
-
-            
             var score = 0;
-            gen[sel].genotype = "";
             var diff = 0;
-            for (var a = 0; a < testSize; a++){
-                var bestScore = 0;
+            var bestScore = 0;
+            for (var a = 0; a < testSize; a++)
+            {
                 var rand = GetRandomNumber(0, 100);
-                gen[rand].genotype = "";
-                for(int b= 0; b < ALLELES; b++)
+                for (var b = 0; b < ALLELES; b++)
                 {
-                    score = 0;
-                    var gene = gen[rand].genes[b].ToString() + gen[rand].genes[b+1].ToString();
-                    var tempDiff = 0;
-                    
-                   // var n = gene.Equals("00");
-                    if (gene.Equals("00"))
-                    {
-                        gen[rand].phenotype = gen[rand].phenotype + "a";
-                        gen[sel].phenotype = gen[sel].phenotype + "a";
-                    }
-                    else if (gene.Equals("01"))
-                    {
-                        gen[rand].phenotype = gen[rand].phenotype + "b";
-                        gen[sel].phenotype = gen[sel].phenotype + "b";
-                    }
-                    else if (gene.Equals("10"))
-                    {
-                        gen[rand].phenotype = gen[rand].phenotype + "c";
-                        gen[sel].phenotype = gen[sel].phenotype + "c";
-                    }
-                    else if(gene.Equals("11"))
-                    {
-                        gen[rand].phenotype = gen[rand].phenotype + "d";
-                        gen[sel].phenotype = gen[sel].phenotype + "d";
-                    }
-
                     var l1 = gen[rand].phenotype[b];
                     var l2 = gen[sel].phenotype[b];
-
+                    var tempDiff = 0;
                     tempDiff = Math.Abs(l1 - l2);
                     diff = diff + tempDiff;
-                    
                 }
                 score = gen[sel].fitness + diff;
-                diff = 0;
-                if(score > bestScore)
+                if (score > bestScore)
                 {
                     bestScore = score;
                     bestPartner = rand;
                 }
-
-
             }
+
             return bestPartner;
         }
 
         //Utiltiy Functions 
 
 
-        public static void copyPopulation(int [,] from)
+        public static void copyPopulation(int[,] from)
         {
-            for(var b = 0; b<POPULATION; b++)
+            for (var b = 0; b < POPULATION; b++)
             {
-                for(var a = 0; a<GENES; a++)
+                for (var a = 0; a < GENES; a++)
                 {
-                    var gene = from[b ,a];
-                  //  GEN[b, a] = gene;           
+                    var gene = from[b, a];
                 }
             }
 
